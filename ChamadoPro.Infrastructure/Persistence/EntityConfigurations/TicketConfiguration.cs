5831260eecf1d5076272b15bab3ee02f25809797
Gabriel Alves <gabrielalvesbosca@gmail.com>
2025-03-06 16:25:39 -0300

    Commit

---
+++ ChamadoPro.Infrastructure/Persistence/EntityConfigurations/TicketConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using ChamadoPro.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChamadoPro.Infrastructure.Persistence.EntityConfigurations
+{
+    public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
+    {
+        public void Configure(EntityTypeBuilder<Ticket> builder)
+        {
+            builder.ToTable("Tickets");
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Title)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(t => t.Description)
+                .IsRequired()
+                .HasMaxLength(1000);
+
+            builder.Property(t => t.Status)
+                .HasConversion<string>()
+                .IsRequired();
+
+            builder.Property(t => t.Priority)
+                .HasConversion<string>()
+                .IsRequired();
+
+            builder.HasOne(t => t.Category)
+                .WithMany()
+                .HasForeignKey(t => t.CategoryId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.HasOne(t => t.Requester)
+                .WithMany()
+                .HasForeignKey(t => t.RequesterId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(t => t.Responsible)
+                .WithMany()
+                .HasForeignKey(t => t.ResponsibleId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.Property(t => t.Date_created)
+                .IsRequired()
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            builder.Property(t => t.ConcludedAt)
+                .IsRequired(false);
+        }
+    }
+}
---
+++ ChamadoPro.Infrastructure/Persistence/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using ChamadoPro.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChamadoPro.Infrastructure.Persistence.EntityConfigurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(255);
+            builder.HasIndex(u => u.Email).IsUnique();
+
+            builder.Property(u => u.Password)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            builder.Property(u => u.Role)
+                .HasConversion<string>()
+                .IsRequired();
+
+            builder.Property(u => u.Status)
+                .HasConversion<string>()
+                .IsRequired();
+        }
+    }
+}
