64d52295623d9aa4b9f8fc6bf38a89a1c6d5b243
Gabriel Alves <gabrielalvesbosca@gmail.com>
2025-03-06 16:07:42 -0300

    Commit

--- ChamadoPro.API/Program.cs
+++ ChamadoPro.API/Program.cs
@@ -1,5 +1,11 @@
+using ChamadoPro.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
