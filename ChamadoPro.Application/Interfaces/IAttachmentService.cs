94c95db61b0679263d565c85d1e1905948def656
Gabriel Alves <gabrielalvesbosca@gmail.com>
2025-03-07 22:22:36 -0300

    Commit

---
+++ ChamadoPro.Application/Interfaces/IAttachmentService.cs
@@ -0,0 +1,14 @@
+﻿using ChamadoPro.Application.DTOs.Attachment;
+using ChamadoPro.Application.DTOs.Category;
+
+namespace ChamadoPro.Application.Interfaces
+{
+    public interface IAttachmentService
+    {
+        Task<IEnumerable<AttachmentResponseDTO>> GetByTicketIdAsync(int ticketId);
+        Task<AttachmentResponseDTO> GetByIdAsync(int id);
+        Task<AttachmentResponseDTO> CreateAsync(AttachmentRequestDTO attachment);
+        Task<AttachmentResponseDTO> UpdateAsync(AttachmentRequestDTO attachment);
+        Task DeleteAsync(int id);
+    }
+}
--- ChamadoPro.Application/Services/TicketService/AttachmentService.cs
+++ ChamadoPro.Application/Services/TicketService/AttachmentService.cs
@@ -1,6 +1,42 @@
-﻿namespace ChamadoPro.Application.Services.TicketService
+﻿using ChamadoPro.Application.DTOs.Attachment;
+using ChamadoPro.Application.Interfaces;
+using ChamadoPro.Domain.Entities;
+using ChamadoPro.Domain.Interfaces;
+
+namespace ChamadoPro.Application.Services.TicketService
 {
-    internal class AttachmentService
+    public class AttachmentService : IAttachmentService
     {
+        private readonly IAttachmentRepository _attachmentRepository;
+
+        public AttachmentService(IAttachmentRepository attachmentRepository)
+        {
+            _attachmentRepository = attachmentRepository;
+        }
+
+        public async Task<AttachmentResponseDTO> CreateAsync(AttachmentRequestDTO attachment)
+        {
+
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AttachmentResponseDTO> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AttachmentResponseDTO>> GetByTicketIdAsync(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AttachmentResponseDTO> UpdateAsync(AttachmentRequestDTO attachment)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
