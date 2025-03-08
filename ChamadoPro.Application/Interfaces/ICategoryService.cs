811eefa1417e2b970036ced53bf9737f93e4c6fc
Gabriel Alves <gabrielalvesbosca@gmail.com>
2025-03-07 22:03:16 -0300

    Commit

---
+++ ChamadoPro.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using ChamadoPro.Application.DTOs.Category;
+
+namespace ChamadoPro.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryResponseDTO>> GetAllAsync();
+        Task<CategoryResponseDTO> GetByIdAsync(int id);
+        Task<CategoryResponseDTO> CreateAsync(CategoryRequestDTO categoryRequest);
+        Task<CategoryResponseDTO> UpdateAsync(int id, CategoryRequestDTO categoryRequest);
+        Task DeleteAsync(int id);
+    }
+}
--- ChamadoPro.Application/Services/CategoryServices/CategoryService.cs
+++ ChamadoPro.Application/Services/CategoryServices/CategoryService.cs
@@ -1,12 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ChamadoPro.Application.DTOs.Category;
+using ChamadoPro.Application.Interfaces;
+using ChamadoPro.Domain.Entities;
+using ChamadoPro.Domain.Interfaces;
 
 namespace ChamadoPro.Application.Services.CategoryServices
 {
-    internal class CategoryService
+    public class CategoryService : ICategoryService
     {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IEnumerable<CategoryResponseDTO>> GetAllAsync()
+        {
+            var categoriesEntity = await _categoryRepository.GetAllAsync();
+
+            return categoriesEntity.Select(category => new CategoryResponseDTO
+            {
+                Id = category.Id,
+                Name = category.Name
+            });
+        }
+
+        public async Task<CategoryResponseDTO> GetByIdAsync(int id)
+        {
+            var categoryEntity = await _categoryRepository.GetByIdAsync(id);
+
+            return new CategoryResponseDTO
+            {
+                Id = categoryEntity.Id,
+                Name = categoryEntity.Name
+            };
+        }
+
+        public async Task<CategoryResponseDTO> CreateAsync(CategoryRequestDTO categoryRequest)
+        {
+            var categoryEntity = new Category
+            {
+                Name = categoryRequest.Name
+            };
+
+            var category = await _categoryRepository.CreateAsync(categoryEntity);
+
+            return new CategoryResponseDTO
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+        }
+
+        public async Task<CategoryResponseDTO> UpdateAsync(int id, CategoryRequestDTO categoryRequest)
+        {
+            var categoryEntity = await _categoryRepository.GetByIdAsync(id);
+
+            if (categoryEntity == null)
+                return null;
+
+            categoryEntity.Name = categoryRequest.Name;
+
+            var category = await _categoryRepository.UpdateAsync(categoryEntity);
+
+            return new CategoryResponseDTO
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _categoryRepository.DeleteAsync(id);
+        }
     }
 }
