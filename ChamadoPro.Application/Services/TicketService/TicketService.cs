fb1f57685376dc362ed7634fe318266fa7e7167b
Gabriel Alves <gabrielalvesbosca@gmail.com>
2025-03-10 22:27:12 -0300

    Commit

--- ChamadoPro.Application/Services/TicketService/TicketService.cs
+++ ChamadoPro.Application/Services/TicketService/TicketService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChamadoPro.Application.Services.TicketService
+﻿namespace ChamadoPro.Application.Services.TicketService
 {
     internal class TicketService
     {
