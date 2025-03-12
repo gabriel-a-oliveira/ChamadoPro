c120377526a579423829e135f22cd5be4556cd67
Gabriel Alves <gabrielalvesbosca@gmail.com>
2025-03-11 22:04:49 -0300

    Commit

---
+++ ChamadoPro.API/Controllers/AttachmentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChamadoPro.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttachmentController : ControllerBase
+    {
+    }
+}
