e5017cc6aefc93c1aaff7609030d53c662b49c39
Gabriel Alves <gabrielalvesbosca@gmail.com>
2025-03-12 22:42:34 -0300

    Commit

--- ChamadoPro.API/Controllers/UserController.cs
+++ ChamadoPro.API/Controllers/UserController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using ChamadoPro.Application.DTOs.User;
+using ChamadoPro.Application.Interfaces;
 
 namespace ChamadoPro.API.Controllers
 {
@@ -7,6 +8,54 @@ namespace ChamadoPro.API.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserService _userService;
 
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserResponseDTO>>> GetAll()
+        {
+            var users = await _userService.GetAllAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserResponseDTO>> GetById(int id)
+        {
+            var user = await _userService.GetByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserResponseDTO>> Create([FromBody] UserRequestDTO userRequest)
+        {
+            var newUser = await _userService.CreateAsync(userRequest);
+            return CreatedAtAction(nameof(GetById), new { id = newUser.Id }, newUser);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UserResponseDTO>> Update(int id, [FromBody] UserRequestDTO userRequest)
+        {
+            var updatedUser = await _userService.UpdateAsync(id, userRequest);
+            if (updatedUser == null)
+            {
+                return NotFound();
+            }
+            return Ok(updatedUser);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _userService.DeleteAsync(id);
+            return NoContent();
+        }
     }
 }
