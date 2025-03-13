483a00fdc2ca3a74d93f127b8552f2ec42f24f79
Gabriel Alves <gabrielalvesbosca@gmail.com>
2025-03-13 20:49:43 -0300

    Commit

---
+++ ChamadoPro.Infrastructure/Migrations/20250313234713_DataCreatedTicket.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChamadoPro.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataCreatedTicket : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date_created",
+                table: "Tickets",
+                newName: "DateCreated");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateCreated",
+                table: "Tickets",
+                newName: "Date_created");
+        }
+    }
+}
