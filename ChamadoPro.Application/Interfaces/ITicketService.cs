ae795b41816669c396aa27e4a44e08c9b6972814
Gabriel Alves <gabrielalvesbosca@gmail.com>
2025-03-13 20:32:47 -0300

    Commit

---
+++ ChamadoPro.Application/Interfaces/ITicketService.cs
@@ -0,0 +1,20 @@
+﻿using ChamadoPro.Domain.Entities;
+using ChamadoPro.Domain.Enums;
+
+namespace ChamadoPro.Application.Interfaces
+{
+    public interface ITicketService
+    {
+        Task<Ticket?> GetTicketByIdAsync(int id);
+        Task<IEnumerable<Ticket>> GetTicketsByUserIdAsync(int userId);
+        Task<IEnumerable<Ticket>> GetTicketsByResponsibleAsync(int responsibleId);
+        Task<IEnumerable<Ticket>> GetTicketsByStatusAsync(Status status);
+        Task<IEnumerable<Ticket>> GetTicketsByPriorityAsync(Priority priority);
+        Task<IEnumerable<Ticket>> GetTicketsByCategoryAsync(int categoryId);
+        Task<IEnumerable<Ticket>> GetTicketsByCompletedAsync(DateTime dateTime);
+        Task<IEnumerable<Ticket>> GetAllTicketsAsync();
+        Task<Ticket> CreateTicketAsync(Ticket ticket);
+        Task<Ticket> UpdateTicketAsync(Ticket ticket);
+        Task DeleteTicketAsync(int id);
+    }
+}
