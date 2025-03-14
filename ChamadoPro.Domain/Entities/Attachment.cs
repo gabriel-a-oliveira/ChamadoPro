67f5ff6676193ef08c97ef858966a2bb0079af99
Gabriel Alves <gabrielalvesbosca@gmail.com>
2025-03-13 21:11:49 -0300

    Commit

--- ChamadoPro.Domain/Entities/Attachment.cs
+++ ChamadoPro.Domain/Entities/Attachment.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public int TicketId { get; set; }
-        public Ticket Ticket { get; set; }
-        public string Name { get; set; }
-        public string FileUrl { get; set; }
+        public Ticket? Ticket { get; set; }
+        public string? Name { get; set; }
+        public string? FileUrl { get; set; }
         public DateTime DateCreated { get; set; }
     }
 }
--- ChamadoPro.Domain/Entities/Ticket.cs
+++ ChamadoPro.Domain/Entities/Ticket.cs
@@ -5,8 +5,8 @@ namespace ChamadoPro.Domain.Entities
     public class Ticket
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
         public Status Status { get; set; }
         public Priority Priority { get; set; }
         public int? CategoryId { get; set; }
